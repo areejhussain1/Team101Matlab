b6aa8aab9bff76cfbf0709bc236de4a8154863ab
AJ <AJ@aj>
2026-02-10 15:01:04 -0500

    Start of data analysis branch, initial flow in comments

---
+++ DataAnalysis.m
@@ -0,0 +1,8 @@
+% This file will Analyze all the experimental data
+% 
+% * look at all the data in data/
+% 
+% * combine (N) different trials of the same material into a [4xN] matrix
+%   or a struct based on naming convention
+% 
+% * 
