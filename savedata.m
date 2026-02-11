eff2d48b359003536399517ac5692651621a30ef
AJ <AJ@aj>
2026-02-11 12:38:07 -0500

    sc poly data

--- savedata.m
+++ savedata.m
@@ -1,4 +1,4 @@
-titlesave = "Data\iso1-4_70" + string(datestr(now, 'yyyymmdd_HHMMSS'));
+titlesave = "Data\iso1-4_50" + string(datestr(now, 'yyyymmdd_HHMMSS'));
 save(titlesave,"t_sec","ax_g","ay_g","az_g")
 
 % ie title name: neo1-8_60A
