707c788d22edc59bd913d2c2aa555f0926f6a004
AJ <AJ@aj>
2026-02-11 12:49:50 -0500

    updated pseudocode

--- DataAnalysis.m
+++ DataAnalysis.m
@@ -5,4 +5,5 @@
 % * combine (N) different trials of the same material into a [4xN] matrix
 %   or a struct based on naming convention
 % 
-% * 
+% * run plotLoadedData for each trial, extract damping ratio and other
+%   important aspects
